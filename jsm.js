8510daecd5c030da38d1c8fbdcaaa5d55d11c28a
Oluwatosin Oguntoyinbo <oluwatosinoguntoyinboo@gmail.com>
2024-11-21 01:46:44 +0100

    Retesting

--- jsm.js
+++ jsm.js
@@ -40,11 +40,11 @@ form.addEventListener('submit', (event) => {
 
   // You can add more complex validation here, such as email format checking,
   // input length restrictions, etc.
-});
+});*/
 
 function myFunction(){
     window.location.href="https://3mtt-c2-hack-jsm.vercel.app/login.html";
-}*/
+}
 
 
 
